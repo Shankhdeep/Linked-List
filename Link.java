d685d720532ceba795f7a0eb1a74087d5974f201
Shankhdeep <97821571+Shankhdeep@users.noreply.github.com>
2022-07-08 10:21:23 +0530

    Create Link.java

---
+++ Link.java
@@ -0,0 +1,15 @@
+import java.util.*;
+	
+public class Link {
+	
+	public static void main(String args[])
+	{
+		LinkedList<String> ll = new LinkedList<>();
+	
+		ll.add("Hi");
+		ll.add("Shankhdeep");
+		ll.add(1, "I am");
+	
+		System.out.println(ll);
+	}
+}
